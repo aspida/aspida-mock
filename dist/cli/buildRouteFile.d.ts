fb06b410e89cf386b59d5b97eac97f531f955d0c
solufa <solufa2020@gmail.com>
2020-08-30 03:14:58 +0900

    chore: delete dist

--- dist/cli/buildRouteFile.d.ts
+++
@@ -1,7 +0,0 @@
-import { AspidaConfig } from 'aspida/dist/commands';
-declare const _default: ({ input, trailingSlash }: AspidaConfig) => {
-    text: string;
-    filePath: string;
-};
-export default _default;
-//# sourceMappingURL=buildRouteFile.d.ts.map
